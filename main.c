746be72a6a98c330af7eaf393b865f3728f9a6ce
Hope Whitelock <hope.whitelock@uconn.edu>
2015-12-17 14:25:18 -0500

    first commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+#include "integrand.c"
+
+#include <stdlib.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_monte.h>
+#include <gsl/gsl_monte_plain.h>
+#include <gsl/gsl_monte_miser.h>
+#include <gsl/gsl_monte_vegas.h>
+#include <gsl/gsl_math.h>
+int main (void)
+{
+	
+	
+	return 0;
+}
